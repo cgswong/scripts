e384f6c3b4935197b3165d1033d69f816147ab59
Stuart Wong <cgs.wong@gmail.com>
2016-04-26 13:12:17 -0500

    chore: add AWS and Kubernetes scripts
    Add AWS scripts for S3 and Assume Role (for Terraform).
    Add Kubernetes setup scripts.

---
+++ aws_assume_role.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CMD="$@"
+: ${AWS_ACCOUNT_ID:="265365382492"}
+: ${AWS_ROLE_NAME:="datahub"}
+: ${AWS_PROFILE_NAME:="datahub-np"}
+: ${ROLE_SESSION_NAME:="nonprd"}
+
+ASSUME_ROLE="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ROLE_NAME}"
+TMP_FILE="/tmp/.aws-credentials.tmp"
+
+aws --profile=${AWS_PROFILE_NAME} sts assume-role --output json --role-arn ${ASSUME_ROLE} --role-session-name ${ROLE_SESSION_NAME} &> ${TMP_FILE}
+
+ACCESS_KEY=$(cat ${TMP_FILE} | jq -r ".Credentials.AccessKeyId")
+SECRET_KEY=$(cat ${TMP_FILE} | jq -r ".Credentials.SecretAccessKey")
+SESSION_TOKEN=$(cat ${TMP_FILE} | jq -r ".Credentials.SessionToken")
+EXPIRATION=$(cat ${TMP_FILE} | jq -r ".Credentials.Expiration")
+
+echo "Retrieved temp access key ${ACCESS_KEY} for role ${ASSUME_ROLE}. Key will expire at ${EXPIRATION}"
+
+TF_VAR_aws_access_key=${ACCESS_KEY} TF_VAR_aws_secret_key=${SECRET_KEY} TF_VAR_aws_session_token=${SESSION_TOKEN} ${CMD}
--- k8-setup.sh
+++ k8-setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 # Configure local Kubernetes client, kubectl, to access local Vagrant Kubernetes.
 
-: ${REPO:="~/repos/git/coreos-kubernetes/multi-node/vagrant"}
-: ${CA:="${REPO}/ssl/ca.pem"}
-: ${adminKey:="${REPO}/ssl/admin-key.pem"}
-: ${IPADDR:="172.17.4.101"}
-: ${TGT:="vagrant"}
+: ${K8_REPO:="~/repos/git/coreos-kubernetes/multi-node/vagrant"}
+: ${K8_CA:="${K8_REPO}/ssl/ca.pem"}
+: ${K8_ADMKEY:="${K8_REPO}/ssl/admin-key.pem"}
+: ${K8_IPADDR:="172.17.4.101"}
+: ${K8_PORT:=443}
+: ${K8_TGT:="vagrant-multi"}
 
-cd ${REPO}
-kubectl config set-cluster ${TGT} --server=https://${IPADDR}:443 --certificate-authority=${CA}
-kubectl config set-credentials vagrant-admin --certificate-authority=${CA} --client-key=${adminKey} --client-certificate=${PWD}/ssl/admin.pem
-kubectl config set-context ${TGT} --cluster=${TGT} --user=vagrant-admin
-kubectl config use-context ${TGT}
+cd ${K8_REPO}
+kubectl config set-cluster ${K8_TGT}-cluster --server=https://${K8_IPADDR}:${K8_PORT} --certificate-authority=${K8_CA}
+kubectl config set-credentials ${K8_TGT}-admin --certificate-authority=${K8_CA} --client-key=${K8_ADMKEY} --client-certificate=${PWD}/ssl/admin.pem
+kubectl config set-context ${K8_TGT} --cluster=${K8_TGT}-cluster --user=${K8_TGT}-admin
+kubectl config use-context ${K8_TGT}
